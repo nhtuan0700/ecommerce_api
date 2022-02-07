afbbaa3410b964be82a045053933208ca4b0c9ca
TuanNguyen <nhtuan0700@gmail.com>
2022-02-07 16:44:35 +0700

    Init Project

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['as' => 'product.', 'prefix' => 'product'], function () {
+    Route::controller(ProductController::class)->group(function () {
+        Route::get('/', 'index');
+    });
+});
+
+Route::group(['as' => 'user.', 'prefix' => 'user'], function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/', 'index');
+    });
+});
